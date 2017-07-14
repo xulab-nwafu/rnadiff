84d045f4d415a1ec18651298ec4bbcfd00bba478
Qinhu Wang <qinhu.wang@icloud.com>
2017-07-14 17:35:25 +0800

    add rnadiff

--- rnaseq.R
+++ rnaseq.R
@@ -1,9 +1,20 @@
-rm(list=ls())
+library(methods)
+args <- commandArgs(trailingOnly = TRUE)
+target_file <- args[1]
+rnaseq_file <- args[2]
+control_tag <- args[3]
+case_tag    <- args[4]
+
+# target_file <- "demo/targets.txt"
+# rnaseq_file <- "demo/rnaseq.tsv"
+# control_tag <- "wt"
+# case_tag    <- "mu"
+
 library(edgeRun)
 
 # load data
-targets <- readTargets("targets.txt")
-x<-read.delim("rnaseq.tsv", row.names=1, stringsAsFactors=FALSE, as.is = TRUE)
+targets <- readTargets(file = target_file)
+x<-read.delim(file = rnaseq_file, row.names=1, stringsAsFactors=FALSE, as.is = TRUE, sep = "\t")
 
 # Preprocess
 y <- DGEList(counts=x, group=targets$Treatment)
@@ -15,14 +26,14 @@ y <- estimateCommonDisp(y, verbose=TRUE)
 y <- estimateTagwiseDisp(y)
 
 # QC plot
-pdf("QC.pdf")
+pdf("edgeRun.dir/QC.pdf")
 pairs(x, pch=".", upper.panel = NULL)
 plotBCV(y)
 plotMDS(y, method = "bcv")
 dev.off()
 
 # DEG
-z <- UCexactTest(y,pair=c("wt","mu"))
+z <- UCexactTest(y, pair=c("wt", "mu"))
 de <- decideTestsDGE(z, adjust.method = "BH", p.value = 0.05, lfc = 1)
 summary(de)
 
@@ -30,12 +41,12 @@ summary(de)
 detags <- rownames(y)[as.logical(de)]
 uptags<-rownames(y)[as.logical(de>0)]
 downtags<-rownames(y)[as.logical(de<0)]
-write.table(detags, "de.txt", quote=F,col.names = F, row.names=F)
-write.table(downtags, "de.down.txt", quote=F,col.names = F, row.names=F)
-write.table(uptags, "de.up.txt", quote=F,col.names = F, row.names=F)
+write.table(detags, "edgeRun.dir/de.txt", quote=F,col.names = F, row.names=F)
+write.table(downtags, "edgeRun.dir/de.down.txt", quote=F,col.names = F, row.names=F)
+write.table(uptags, "edgeRun.dir/de.up.txt", quote=F,col.names = F, row.names=F)
 
 # MA plot
-pdf("MA.pdf")
+pdf("edgeRun.dir/MA.pdf")
 plotSmear(z, de.tags=detags)
 dev.off()
 
@@ -45,7 +56,7 @@ diff <- topTags(z, n = nrow(y))
 expr <-cpm(y)[rownames(diff),]
 # Summary table
 sum<-cbind(diff$table, expr)
-write.table(sum, "rnaseq.sum.tsv", quote=F, col.names=NA, row.names = TRUE, sep="\t")
+write.table(sum, "edgeRun.dir/rnaseq.sum.tsv", quote=F, col.names=NA, row.names = TRUE, sep="\t")
 
 # Save image
-save.image(file = "rnaseq.RData")
+save.image(file = "edgeRun.dir/rnaseq.RData")
